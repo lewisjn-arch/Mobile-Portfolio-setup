3afd890ec9ce4e558e56c346bb62725f5e0ac5a9
lewisjn-arch <lhinga75@gmail.com>
2023-06-20 14:41:06 +0300

    remove logo from mobile menu

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const burgerMenu = document.querySelector('.burger_menu');
 const navMenu = document.querySelector('.nav_menu');
+const navLogo = document.querySelector('.nav_bar_logo');
+const navBar = document.querySelector('.nav_bar_container');
 
 burgerMenu.addEventListener('click', () => {
   burgerMenu.classList.toggle('active');
   navMenu.classList.toggle('active');
+  navLogo.classList.toggle('active');
+  navBar.classList.toggle('active');
 });
 
 document.querySelectorAll('.nav_links').forEach((n) => n.addEventListener('click', () => {
