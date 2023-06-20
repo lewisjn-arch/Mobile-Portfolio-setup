1b39b7dbdd10b5ecec441e6d6d786b282240d7bb
lewisjn-arch <lhinga75@gmail.com>
2023-06-20 13:41:45 +0300

    install linters, fix linter errors

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const burgerMenu = document.querySelector(".burger_menu");
-const navMenu = document.querySelector(".nav_menu");
+const burgerMenu = document.querySelector('.burger_menu');
+const navMenu = document.querySelector('.nav_menu');
 
-burgerMenu.addEventListener("click", () => {
-    burgerMenu.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
+burgerMenu.addEventListener('click', () => {
+  burgerMenu.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
 
-document.querySelectorAll(".nav_links").forEach(n => n.addEventListener("click", () => {
-    burgerMenu.classList.remove("active");
-    navMenu.classList.remove("active");
+document.querySelectorAll('.nav_links').forEach((n) => n.addEventListener('click', () => {
+  burgerMenu.classList.remove('active');
+  navMenu.classList.remove('active');
 }));
