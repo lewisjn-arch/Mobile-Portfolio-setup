13276ed132708ff6a85e7d14bd2b378886c92d6c
Cheptoo Patience <95401774+Cheptoo21@users.noreply.github.com>
2023-06-29 18:06:19 +0300

    fix eslint errors

--- Javascript/storage.js
+++ Javascript/storage.js
@@ -4,21 +4,21 @@ const textInput = document.querySelector('#text-area');
 
 function storeData() {
     const formData = {
-        username: nameInput.value,
-        email: emailInput.value,
-        text: textInput.value,
-    };
-    localStorage.setItem('formData', JSON.stringify(formData));
+    username: nameInput.value,
+    email: emailInput.value,
+    text: textInput.value,
+  };
+  localStorage.setItem('formData', JSON.stringify(formData));
 }
+
 const inputSaved = [nameInput, emailInput, textInput];
 inputSaved.forEach((input) => {
-input.addEventListener('input', storeData);
+  input.addEventListener('input', storeData);
 });
 
-const savedData = JSON.parse(localStorage.getItem('formData'))
-if(savedData) {
+const savedData = JSON.parse(localStorage.getItem('formData'));
+if (savedData) {
   nameInput.value = savedData.username;
   emailInput.value = savedData.email;
   textInput.value = savedData.text;
-};
-
+}
