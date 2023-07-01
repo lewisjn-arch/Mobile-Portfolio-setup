89a292ce1ff27757d995d742236ccb3ccf99d3d7
lewisjn-arch <lhinga75@gmail.com>
2023-07-02 01:13:42 +0300

    add popup functionality

--- Javascript/popup.js
+++ Javascript/popup.js
@@ -1,92 +1,117 @@
+// Projects info
 const projects = [
   {
     title: 'E-commerce Website',
     previewDescription: 
-    "A daily selection of privately personalized reads; no accounts or sign-ups required." + 
-    " This Has been the industry's standard.",
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry." + 
-    " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown " +
-    "printer took a galley of type and scrambled it to make a type specimen book." + 
-    " It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    `A daily selection of privately personalized reads; no accounts or sign-ups required.
+    This Has been the industry's standard.`,
+    description_p1:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
+    galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of 
+    type and scrambled it 1960s with the releax map lapora verita.`,
+    description_p2:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
+    galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry.`,
     projectImage: 'Assets/Images/ecommerce.png',
     technologies: ['HTML', 'Bootstrap', 'Ruby'],
-    liveVersion: '#',
-    sourceCode: '#',
+    liveVersion: 'https://lewisjn-arch.github.io/ArchLewisHinga.github.io/',
+    sourceCode: 'https://github.com/lewisjn-arch/Mobile-Portfolio-setup',
   },
   {
     title: 'University Portal',
     previewDescription: 
-    "A daily selection of privately personalized reads; no accounts or sign-ups required." + 
-    " This Has been the industry's standard.",
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry." + 
-    " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown " +
-    "printer took a galley of type and scrambled it to make a type specimen book." + 
-    " It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    `A daily selection of privately personalized reads; no accounts or sign-ups required.
+    This Has been the industry's standard.`,
+    description_p1:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
+    galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of 
+    type and scrambled it 1960s with the releax map lapora verita.`,
+    description_p2:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
+    galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry.`,
     projectImage: 'Assets/Images/university.png',
     technologies: ['HTML', 'Bootstrap', 'Ruby'],
-    liveVersion: '#',
-    sourceCode: '#',
+    liveVersion: 'https://lewisjn-arch.github.io/ArchLewisHinga.github.io/',
+    sourceCode: 'https://github.com/lewisjn-arch/Mobile-Portfolio-setup',
   },
   {
     title: 'Company Website',
     previewDescription: 
-    "A daily selection of privately personalized reads; no accounts or sign-ups required." + 
-    " This Has been the industry's standard.",
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry." + 
-    " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown " +
-    "printer took a galley of type and scrambled it to make a type specimen book." + 
-    " It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    `A daily selection of privately personalized reads; no accounts or sign-ups required.
+    This Has been the industry's standard.`,
+    description_p1:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
+    galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of 
+    type and scrambled it 1960s with the releax map lapora verita.`,
+    description_p2:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
+    galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry.`,
     projectImage: 'Assets/Images/company.jpg',
     technologies: ['HTML', 'Bootstrap', 'Ruby'],
-    liveVersion: '#',
-    sourceCode: '#',
+    liveVersion: 'https://lewisjn-arch.github.io/ArchLewisHinga.github.io/',
+    sourceCode: 'https://github.com/lewisjn-arch/Mobile-Portfolio-setup',
   },
   {
     title: 'Business Website',
     previewDescription: 
-    "A daily selection of privately personalized reads; no accounts or sign-ups required." + 
-    " This Has been the industry's standard.",
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry." + 
-    " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown " +
-    "printer took a galley of type and scrambled it to make a type specimen book." + 
-    " It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    `A daily selection of privately personalized reads; no accounts or sign-ups required.
+    This Has been the industry's standard.`,
+    description_p1:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
+    galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of 
+    type and scrambled it 1960s with the releax map lapora verita.`,
+    description_p2:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
+    galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry.`,
     projectImage: 'Assets/Images/business.jpg',
     technologies: ['HTML', 'Bootstrap', 'Ruby'],
-    liveVersion: '#',
-    sourceCode: '#',
+    liveVersion: 'https://lewisjn-arch.github.io/ArchLewisHinga.github.io/',
+    sourceCode: 'https://github.com/lewisjn-arch/Mobile-Portfolio-setup',
   },
   {
     title: 'Bank Website',
     previewDescription: 
-    "A daily selection of privately personalized reads; no accounts or sign-ups required." + 
-    " This Has been the industry's standard.",
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry." + 
-    " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown " +
-    "printer took a galley of type and scrambled it to make a type specimen book." + 
-    " It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    `A daily selection of privately personalized reads; no accounts or sign-ups required.
+    This Has been the industry's standard.`,
+    description_p1:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
+    galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of 
+    type and scrambled it 1960s with the releax map lapora verita.`,
+    description_p2:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
+    galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry.`,
     projectImage: 'Assets/Images/banking.jpg',
     technologies: ['HTML', 'Bootstrap', 'Ruby'],
-    liveVersion: '#',
-    sourceCode: '#',
+    liveVersion: 'https://lewisjn-arch.github.io/ArchLewisHinga.github.io/',
+    sourceCode: 'https://github.com/lewisjn-arch/Mobile-Portfolio-setup',
   },
   {
     title: 'Trading Website',
     previewDescription: 
-    "A daily selection of privately personalized reads; no accounts or sign-ups required." + 
-    " This Has been the industry's standard.",
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry." + 
-    " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown " +
-    "printer took a galley of type and scrambled it to make a type specimen book." + 
-    " It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    `A daily selection of privately personalized reads; no accounts or sign-ups required.
+    This Has been the industry's standard.`,
+    description_p1:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
+    galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of 
+    type and scrambled it 1960s with the releax map lapora verita.`,
+    description_p2:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
+    galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry.`,
     projectImage: 'Assets/Images/trading.jpg',
     technologies: ['HTML', 'Bootstrap', 'Ruby'],
-    liveVersion: '#',
-    sourceCode: '#',
+    liveVersion: 'https://lewisjn-arch.github.io/ArchLewisHinga.github.io/',
+    sourceCode: 'https://github.com/lewisjn-arch/Mobile-Portfolio-setup',
   },
 ]
 
 const thumbnailContainer = document.querySelector('.portfolio_thumbnails');
-
+// Dynamic project card content
 const displayData = () => {
     projects.map((projectData) => {
       const projectContainer = document.createElement('div');
@@ -117,5 +142,52 @@ const displayData = () => {
 }
 
 displayData();
+
+const seeProjectButton = document.querySelectorAll('.project_link');
+const Portfolio = document.querySelector('.portfolio');
+
+let i = 0;
+seeProjectButton.forEach((btn) => {
+  i += 1;
+
+  const popup = document.createElement('div');
+  popup.classList.add('popup_window');
+  popup.innerHTML = `
+            <div class="popup_title_container">
+               <h2 class="popup_title">${projects[i - 1].title}</h2>
+               <img id="cross-icon"src="Assets/Icons/close.png">
+            </div>
+            <ul class="languages">
+                    <li class="texts">${projects[i - 1].technologies[0]}</li>
+                    <li class="texts">${projects[i - 1].technologies[1]}</li>
+                    <li class="texts">${projects[i - 1].technologies[2]}</li>
+            </ul>
+            <div class="popup_content">
+               <img src="${projects[i - 1].projectImage}" class="image-preview">
+               <div class="popup_info"> 
+                   <p class="main_description">${projects[i - 1].description_p1}</p>
+                   <p class="main_description">${projects[i - 1].description_p2}</p>
+                   <div class="button_container">
+                     <button type="button" class="popup_button" onclick="window.location.href='${projects[i - 1].liveVersion}';">See live<img src="Assets/Icons/Icon.svg" alt="icons" class="btn-icons"></button>
+                     <button type="button" class="popup_button" onclick="window.location.href='${projects[i - 1].sourceCode}';">See source<img src="Assets/Icons/Vector.svg" alt="icons" class="btn-icons"></button>
+                   </div>
+               </div>
+            </div>`;
+            
+
+  btn.addEventListener('click', () => {
+    
+    thumbnailContainer.appendChild(popup);
+
+    document.querySelector('#cross-icon').addEventListener('click', () => {
+      thumbnailContainer.removeChild(popup);
+      document.querySelector('body').style.overflow = 'scroll';
+    });
+  });
+});
+
+
+
+
 
 
