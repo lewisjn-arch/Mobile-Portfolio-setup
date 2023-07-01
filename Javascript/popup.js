16ca19226e2abfcaed7e5f3d8320cf628937a2b7
lewisjn-arch <lhinga75@gmail.com>
2023-07-01 16:09:43 +0300

    add dynamic project cards

--- Javascript/popup.js
+++ Javascript/popup.js
@@ -1,104 +1,117 @@
-let mainPage = document.querySelector('.page_container');
-let bio = document.querySelector('.about_me');
-
 const projects = [
-    {
-      name: 'Architecture',
-      featureDescription: 
-      "A daily selection of privately personalized reads; no accounts or sign-ups required." +
-      " This has been the industry's standard dummy text ever since the 1500s, when an" +
-      " unknown printer took a standard dummy text.",
-      description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry." + 
-      " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown " +
-      "printer took a galley of type and scrambled it to make a type specimen book." + 
-      " It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
-      projectImage: 'Architectural.jpg',
-      technologies: ['Css', 'HTML', 'Bootstrap', 'Ruby'],
-      liveVersion: '#',
-      sourceCode: '#',
-    },
-    {
-      name: 'E-commerce Website',
-      previewDescription: 
-      "A daily selection of privately personalized reads; no accounts or sign-ups required." + 
-      " This Has been the industry's standard.",
-      description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry." + 
-      " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown " +
-      "printer took a galley of type and scrambled it to make a type specimen book." + 
-      " It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
-      projectImage: 'ecommerce.png',
-      technologies: ['HTML', 'Bootstrap', 'Ruby'],
-      liveVersion: '#',
-      sourceCode: '#',
-    },
-    {
-      name: 'University Portal',
-      previewDescription: 
-      "A daily selection of privately personalized reads; no accounts or sign-ups required." + 
-      " This Has been the industry's standard.",
-      description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry." + 
-      " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown " +
-      "printer took a galley of type and scrambled it to make a type specimen book." + 
-      " It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
-      projectImage: 'university.png',
-      technologies: ['HTML', 'Bootstrap', 'Ruby'],
-      liveVersion: '#',
-      sourceCode: '#',
-    },
-    {
-      name: 'Company Website',
-      previewDescription: 
-      "A daily selection of privately personalized reads; no accounts or sign-ups required." + 
-      " This Has been the industry's standard.",
-      description4:"Lorem Ipsum is simply dummy text of the printing and typesetting industry." + 
-      " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown " +
-      "printer took a galley of type and scrambled it to make a type specimen book." + 
-      " It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
-      projectImage4: 'company.jpg',
-      technologies4: ['HTML', 'Bootstrap', 'Ruby'],
-      liveVersion4: '#',
-      sourceCode4: '#',
-    },
-    {
-      name: 'Business Website',
-      previewDescription: 
-      "A daily selection of privately personalized reads; no accounts or sign-ups required." + 
-      " This Has been the industry's standard.",
-      description5:"Lorem Ipsum is simply dummy text of the printing and typesetting industry." + 
-      " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown " +
-      "printer took a galley of type and scrambled it to make a type specimen book." + 
-      " It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
-      projectImage5: 'business.jpg',
-      technologies5: ['HTML', 'Bootstrap', 'Ruby'],
-      liveVersion5: '#',
-      sourceCode5: '#',
-    },
-    {
-      name: 'Bank Website',
-      previewDescription: 
-      "A daily selection of privately personalized reads; no accounts or sign-ups required." + 
-      " This Has been the industry's standard.",
-      description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry." + 
-      " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown " +
-      "printer took a galley of type and scrambled it to make a type specimen book." + 
-      " It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
-      projectImage: 'banking.jpg',
-      technologies: ['HTML', 'Bootstrap', 'Ruby'],
-      liveVersion: '#',
-      sourceCode: '#',
-    },
-    {
-      name: 'Trading Website',
-      previewDescription: 
-      "A daily selection of privately personalized reads; no accounts or sign-ups required." + 
-      " This Has been the industry's standard.",
-      description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry." + 
-      " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown " +
-      "printer took a galley of type and scrambled it to make a type specimen book." + 
-      " It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
-      projectImage: 'trading.jpg',
-      technologies: ['HTML', 'Bootstrap', 'Ruby'],
-      liveVersion: '#',
-      sourceCode: '#',
-    },
-  ];
+  {
+    title: 'E-commerce Website',
+    previewDescription: 
+    "A daily selection of privately personalized reads; no accounts or sign-ups required." + 
+    " This Has been the industry's standard.",
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry." + 
+    " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown " +
+    "printer took a galley of type and scrambled it to make a type specimen book." + 
+    " It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    projectImage: 'Assets/Images/ecommerce.png',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveVersion: '#',
+    sourceCode: '#',
+  },
+  {
+    title: 'University Portal',
+    previewDescription: 
+    "A daily selection of privately personalized reads; no accounts or sign-ups required." + 
+    " This Has been the industry's standard.",
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry." + 
+    " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown " +
+    "printer took a galley of type and scrambled it to make a type specimen book." + 
+    " It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    projectImage: 'Assets/Images/university.png',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveVersion: '#',
+    sourceCode: '#',
+  },
+  {
+    title: 'Company Website',
+    previewDescription: 
+    "A daily selection of privately personalized reads; no accounts or sign-ups required." + 
+    " This Has been the industry's standard.",
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry." + 
+    " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown " +
+    "printer took a galley of type and scrambled it to make a type specimen book." + 
+    " It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    projectImage: 'Assets/Images/company.jpg',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveVersion: '#',
+    sourceCode: '#',
+  },
+  {
+    title: 'Business Website',
+    previewDescription: 
+    "A daily selection of privately personalized reads; no accounts or sign-ups required." + 
+    " This Has been the industry's standard.",
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry." + 
+    " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown " +
+    "printer took a galley of type and scrambled it to make a type specimen book." + 
+    " It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    projectImage: 'Assets/Images/business.jpg',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveVersion: '#',
+    sourceCode: '#',
+  },
+  {
+    title: 'Bank Website',
+    previewDescription: 
+    "A daily selection of privately personalized reads; no accounts or sign-ups required." + 
+    " This Has been the industry's standard.",
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry." + 
+    " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown " +
+    "printer took a galley of type and scrambled it to make a type specimen book." + 
+    " It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    projectImage: 'Assets/Images/banking.jpg',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveVersion: '#',
+    sourceCode: '#',
+  },
+  {
+    title: 'Trading Website',
+    previewDescription: 
+    "A daily selection of privately personalized reads; no accounts or sign-ups required." + 
+    " This Has been the industry's standard.",
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry." + 
+    " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown " +
+    "printer took a galley of type and scrambled it to make a type specimen book." + 
+    " It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    projectImage: 'Assets/Images/trading.jpg',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    liveVersion: '#',
+    sourceCode: '#',
+  },
+]
+
+// const portfolio = document.querySelector('.portfolio')
+const thumbnailContainer = document.querySelector('.portfolio_thumbnails');
+// portfolio.appendChild(thumbnailContainer);
+
+const displayData = () => {
+    projects.map((projectData) => {
+      const projectContainer = document.createElement('div');
+      projectContainer.className = 'project_container';
+      projectContainer.innerHTML = `
+                                <div class="project_title">
+                                    <h3>name</h3>
+                                </div>
+                                <div class="project_description">
+                                    <p>description</p>
+                                </div>
+                                <div class="project_image"></div>
+                                <ul class="project_skills">
+                                    <li class="skill_container"><span class="skill">HTML</span></li>
+                                    <li class="skill_container"><span class="skill">Bootstrap</span></li>
+                                    <li class="skill_container"><span class="skill">Ruby</span></li>
+                                </ul>
+                                <button type="button" class="project_link"><span class="link_text">See project</span></button>
+                             `;
+      thumbnailContainer.appendChild(projectContainer);
+    });
+}
+
+displayData();
+
+
