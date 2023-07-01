1e60d05c8feba838cd78cc87cc1e9de5849212fb
lewisjn-arch <lhinga75@gmail.com>
2023-07-02 01:22:01 +0300

    update popup.js

--- Javascript/popup.js
+++ Javascript/popup.js
@@ -10,9 +10,6 @@ const projects = [
     galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of 
     type and scrambled it 1960s with the releax map lapora verita.`,
-    description_p2:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
-    galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry.`,
     projectImage: 'Assets/Images/ecommerce.png',
     technologies: ['HTML', 'Bootstrap', 'Ruby'],
     liveVersion: 'https://lewisjn-arch.github.io/ArchLewisHinga.github.io/',
@@ -28,9 +25,6 @@ const projects = [
     galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of 
     type and scrambled it 1960s with the releax map lapora verita.`,
-    description_p2:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
-    galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry.`,
     projectImage: 'Assets/Images/university.png',
     technologies: ['HTML', 'Bootstrap', 'Ruby'],
     liveVersion: 'https://lewisjn-arch.github.io/ArchLewisHinga.github.io/',
@@ -46,9 +40,6 @@ const projects = [
     galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of 
     type and scrambled it 1960s with the releax map lapora verita.`,
-    description_p2:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
-    galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry.`,
     projectImage: 'Assets/Images/company.jpg',
     technologies: ['HTML', 'Bootstrap', 'Ruby'],
     liveVersion: 'https://lewisjn-arch.github.io/ArchLewisHinga.github.io/',
@@ -64,9 +55,6 @@ const projects = [
     galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of 
     type and scrambled it 1960s with the releax map lapora verita.`,
-    description_p2:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
-    galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry.`,
     projectImage: 'Assets/Images/business.jpg',
     technologies: ['HTML', 'Bootstrap', 'Ruby'],
     liveVersion: 'https://lewisjn-arch.github.io/ArchLewisHinga.github.io/',
@@ -82,9 +70,6 @@ const projects = [
     galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of 
     type and scrambled it 1960s with the releax map lapora verita.`,
-    description_p2:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
-    galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry.`,
     projectImage: 'Assets/Images/banking.jpg',
     technologies: ['HTML', 'Bootstrap', 'Ruby'],
     liveVersion: 'https://lewisjn-arch.github.io/ArchLewisHinga.github.io/',
@@ -100,9 +85,6 @@ const projects = [
     galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of 
     type and scrambled it 1960s with the releax map lapora verita.`,
-    description_p2:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
-    galley of type and scrambled it 1960s.Lorem Ipsum is simply dummy text of the printing and typesetting industry.`,
     projectImage: 'Assets/Images/trading.jpg',
     technologies: ['HTML', 'Bootstrap', 'Ruby'],
     liveVersion: 'https://lewisjn-arch.github.io/ArchLewisHinga.github.io/',
@@ -149,7 +131,9 @@ const Portfolio = document.querySelector('.portfolio');
 let i = 0;
 seeProjectButton.forEach((btn) => {
   i += 1;
-
+  const portfolio = document.querySelector('.portfolio');
+  const bio = document.querySelector('.about_me');
+  const mainPage = document.querySelector('.page_container');
   const popup = document.createElement('div');
   popup.classList.add('popup_window');
   popup.innerHTML = `
@@ -166,7 +150,6 @@ seeProjectButton.forEach((btn) => {
                <img src="${projects[i - 1].projectImage}" class="image-preview">
                <div class="popup_info"> 
                    <p class="main_description">${projects[i - 1].description_p1}</p>
-                   <p class="main_description">${projects[i - 1].description_p2}</p>
                    <div class="button_container">
                      <button type="button" class="popup_button" onclick="window.location.href='${projects[i - 1].liveVersion}';">See live<img src="Assets/Icons/Icon.svg" alt="icons" class="btn-icons"></button>
                      <button type="button" class="popup_button" onclick="window.location.href='${projects[i - 1].sourceCode}';">See source<img src="Assets/Icons/Vector.svg" alt="icons" class="btn-icons"></button>
@@ -176,10 +159,15 @@ seeProjectButton.forEach((btn) => {
             
 
   btn.addEventListener('click', () => {
-    
+    navBar.style.display = 'none';
+    bio.style.filter = 'blur(2px)';
+    mainPage.style.filter = 'blur(2px)';
     thumbnailContainer.appendChild(popup);
 
     document.querySelector('#cross-icon').addEventListener('click', () => {
+      navBar.style.display = 'fixed';
+      mainPage.style.filter = 'blur(0)';
+      bio.style.filter = 'blur(0)';
       thumbnailContainer.removeChild(popup);
       document.querySelector('body').style.overflow = 'scroll';
     });
